287e65e2334ee5658ed543798ac1b8befa267cbf
asttryd <asttrydp@gmail.com>
2024-03-14 20:06:02 -0300

    Add: first commit of the Game Store API, built with Spring BOOT and MySQL.

---
+++ src/main/java/com/generation/hekategames/LojaHekateGamesApplication.java
@@ -0,0 +1,13 @@
+package com.generation.hekategames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaHekateGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaHekateGamesApplication.class, args);
+	}
+
+}
